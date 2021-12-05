28f0651e2168db2ec2e9e43180f65edc0bf140c9
Andy Ehlert <aehlert@credera.com>
2021-12-05 14:32:04 -0600

    Initial solution and test data file for day one

--- src/main/java/days/one/SonarSweep.java
+++ src/main/java/days/one/SonarSweep.java
@@ -1,9 +1,90 @@
 package days.one;
 
-public class SonarSweep {
+import utils.FileReader;
 
-   public static void main(String[] args) {
-      System.out.println("Day 1");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SonarSweep
+{
+   public static final String INPUT_FILE_NAME = "day_1.txt";
+
+   public static void main(String[] args) throws IOException
+   {
+      countDepthIncreases(FileReader.getFileFromResourceAsStream(INPUT_FILE_NAME));
+      countSlidingWindowSumIncreases(FileReader.getFileFromResourceAsStream(INPUT_FILE_NAME));
    }
 
+   private static void countDepthIncreases(InputStream is) throws IOException
+   {
+      InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(streamReader);
+
+      String line;
+      int previousDepth = 0;
+      int numberOfIncreases = 0;
+      boolean firstIteration = true;
+
+      while ((line = reader.readLine()) != null)
+      {
+         int depth = Integer.parseInt(line);
+         if (firstIteration)
+         {
+            firstIteration = false;
+         }
+         else
+         {
+            if (depth > previousDepth)
+            {
+               numberOfIncreases++;
+            }
+         }
+         previousDepth = depth;
+      }
+
+      System.out.println("Part 1 - Number of Depth Increases: " + numberOfIncreases);
+   }
+
+   private static void countSlidingWindowSumIncreases(InputStream is) throws IOException
+   {
+      InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(streamReader);
+
+      String line;
+      Queue<Integer> currentWindow = new LinkedList<>();
+
+      int maxWindowSize = 3;
+      int currentWindowIndex = 0;
+      int previousWindowSum = 0;
+      int numberOfIncreases = 0;
+
+      while ((line = reader.readLine()) != null)
+      {
+         int depth = Integer.parseInt(line);
+         if (currentWindowIndex < maxWindowSize)
+         {
+            currentWindow.add(depth);
+            previousWindowSum += depth;
+            currentWindowIndex++;
+         }
+         else
+         {
+            int depthToRemove = currentWindow.remove();
+            currentWindow.add(depth);
+            int currentWindowSum = previousWindowSum - depthToRemove + depth;
+            if (currentWindowSum > previousWindowSum)
+            {
+               numberOfIncreases++;
+            }
+            previousWindowSum = currentWindowSum;
+         }
+      }
+
+      System.out.println("Part 2 - Number of Sliding Window Sum Increases: " + numberOfIncreases);
+   }
 }
