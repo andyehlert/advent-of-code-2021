bd5c38e91463f19ed42d6ad231385578a8e2a712
Andy Ehlert <aehlert@credera.com>
2021-12-11 11:05:42 -0600

    Initial commit for day 9

---
+++ src/main/java/days/nine/SmokeBasin.java
@@ -0,0 +1,11 @@
+package days.nine;
+
+public class SmokeBasin
+{
+   public static final String INPUT_FILE_NAME = "day_9.txt";
+
+   public static void main(String[] args)
+   {
+      System.out.println("Using data file " + INPUT_FILE_NAME);
+   }
+}
