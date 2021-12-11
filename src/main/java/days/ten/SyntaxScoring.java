eb51b08c1b4ba70065e244b33db6328929513eed
Andy Ehlert <aehlert@credera.com>
2021-12-11 11:08:54 -0600

    Initial commit for day 10

---
+++ src/main/java/days/ten/SyntaxScoring.java
@@ -0,0 +1,11 @@
+package days.ten;
+
+public class SyntaxScoring
+{
+   public static final String INPUT_FILE_NAME = "day_10.txt";
+
+   public static void main(String[] args)
+   {
+      System.out.println("Using data file " + INPUT_FILE_NAME);
+   }
+}
