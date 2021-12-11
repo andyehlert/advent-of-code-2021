af1450404364154b15e055795fc151e7235eac85
Andy Ehlert <aehlert@credera.com>
2021-12-11 11:04:38 -0600

    Initial commit for day 8

---
+++ src/main/java/days/eight/SevenSegmentSearch.java
@@ -0,0 +1,11 @@
+package days.eight;
+
+public class SevenSegmentSearch
+{
+   public static final String INPUT_FILE_NAME = "day_8.txt";
+
+   public static void main(String[] args)
+   {
+      System.out.println("Using data file " + INPUT_FILE_NAME);
+   }
+}
