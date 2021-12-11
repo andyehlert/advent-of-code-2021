9eae3825548e08d98ea0edb4f34b928fdd74ac8b
Andy Ehlert <aehlert@credera.com>
2021-12-11 10:44:12 -0600

    Initial commit for day six

---
+++ src/main/java/days/six/DaySix.java
@@ -0,0 +1,11 @@
+package days.six;
+
+public class DaySix
+{
+   public static final String INPUT_FILE_NAME = "day_6.txt";
+
+   public static void main(String[] args)
+   {
+      System.out.println("Using data file " + INPUT_FILE_NAME);
+   }
+}
