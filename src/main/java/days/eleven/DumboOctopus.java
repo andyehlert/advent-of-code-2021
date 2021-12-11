2893a05cb019da43e9c93e558d76c5df91be10a7
Andy Ehlert <aehlert@credera.com>
2021-12-11 11:10:15 -0600

    Initial commit for day 11

---
+++ src/main/java/days/eleven/DumboOctopus.java
@@ -0,0 +1,11 @@
+package days.eleven;
+
+public class DumboOctopus
+{
+   public static final String INPUT_FILE_NAME = "day_11.txt";
+
+   public static void main(String[] args)
+   {
+      System.out.println("Using data file " + INPUT_FILE_NAME);
+   }
+}
